c7183a44f7d287334c7a614fe18c80f71565b149
cloaksocks <157986562+cloaksocks@users.noreply.github.com>
2024-06-07 18:47:35 +0300

    Search and display GPS geotags from images in the current directory.

--- findgeotags-multiprocessing.py
+++ findgeotags-multiprocessing.py
@@ -2,36 +2,43 @@
 # -*- coding: utf-8 -*-
 # find-geo-tags multiprocessing
 #
-# Search and display GPS geotags from images in the current directory.
-#
 # Поиск и отображение GPS-геотегов на изображениях в текущем каталоге.
 
 from PIL import Image
 import glob, os
 from datetime import datetime
-import pause
 from GPSPhoto import gpsphoto
 import multiprocessing
 from multiprocessing import Pool
 
+# Получаем путь к директории, где находится скрипт
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def search(file):
     if Image:
         try:
+            # Открываем файл изображения
             img = Image.open(file)
+            # Получаем GPS-данные из изображения
             data = gpsphoto.getGPSData(file)
-            print('in file', file, 'GPS tag was founded: ', data['Latitude'], data['Longitude']) 
+            print('В файле', file, 'найден GPS-тег: ', data['Latitude'], data['Longitude'])
         except:
-            pass 
-        
+            pass
+
 if __name__ == '__main__':
+    # Получаем количество ядер процессора
     cpucores = multiprocessing.cpu_count()
+    # Получаем список всех файлов в текущей директории и поддиректориях
     files = glob.glob(dir_path + '/**/*.*', recursive=True)
-    print('\n print exif gps tags recirsively, \n from current directory photos \n')
-    print('Press Any Key To continue')
-    startTime = datetime.now().replace(microsecond=0)
-    pool = Pool(processes=cpucores + 1)
+    print('\n Выводим EXIF GPS теги рекурсивно, \n из фотографий в текущей директории \n')
+    print('Нажмите любую клавишу для продолжения')
     
+    startTime = datetime.now().replace(microsecond=0)
+    # Создаем пул процессов
+    pool = Pool(processes=cpucores + 1)
+    # Применяем функцию поиска к списку файлов
     pool.map(search, files)
     endTime = datetime.now().replace(microsecond=0)
+
+    # Выводим время, затраченное на обработку
+    print(f"Затраченное время: {endTime - startTime}")
